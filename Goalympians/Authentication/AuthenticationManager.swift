d6e524cb911426fc895cd71fdf45b39d7c25addc
BernardScottIII <bernard.scott111@gmail.com>
2025-03-29 14:40:59 -0400

    Added email authentication

---
+++ Goalympians/Authentication/AuthenticationManager.swift
@@ -0,0 +1,8 @@
+//
+//  AuthenticationManager.swift
+//  Goalympians
+//
+//  Created by Bernard Scott on 3/10/25.
+//
+
+import Foundation
---
+++ Goalympians/Authentication/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  Goalympians
+//
+//  Created by Bernard Scott on 3/10/25.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootView()
+}
